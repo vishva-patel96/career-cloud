2e04cc93e36c88f9fcbb8c712cd1bc02d0ce1079
Vishva Patel <1996vishva@gmail.com>
2022-03-10 21:46:25 -0500

    first commit

---
+++ CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
@@ -0,0 +1,152 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class ApplicantEducationRepository : IDataRepository<ApplicantEducationPoco>
+    {
+        public void Add(params ApplicantEducationPoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(""))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                foreach (ApplicantEducationPoco item in items)
+                {
+
+                    cmd.CommandText = @"INSERT INTO [dbo].[Applicant_Educations]
+           ([Id]
+           ,[Applicant]
+           ,[Major]
+           ,[Certificate_Diploma]
+           ,[Start_Date]
+           ,[Completion_Date]
+           ,[Completion_Percent])
+     VALUES
+           (@Id
+           ,@Applicant
+           ,@Major
+           ,@Certificate_Diploma
+           ,@Start_Date
+           ,@Completion_Date
+           ,@Completion_Percent";
+
+                    cmd.Parameters.AddWithValue("@Id", item.Id);
+                    cmd.Parameters.AddWithValue("@Applicant", item.Applicant);
+
+                    cmd.Parameters.AddWithValue("@Major", item.@Major);
+
+                    cmd.Parameters.AddWithValue("@Certificate_Diploma", item.@CertificateDiploma);
+
+                    cmd.Parameters.AddWithValue("@Start_Date", item.StartDate);
+
+                    cmd.Parameters.AddWithValue("@Completion_Date", item.CompletionDate);
+
+                    cmd.Parameters.AddWithValue("@Completion_Percent", item.CompletionPercent);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }  
+        }
+
+        //public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public override IList<ApplicantEducationPoco> GetAll(params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"Select [Id]
+                                  ,[Applicant]
+                                  ,[Major]
+                                  ,[Certificate_Diploma]
+                                  ,[Start_Date]
+                                  ,[Completion_Date]
+                                  ,[Completion_Percent]
+                                  ,[Time_Stamp]
+                                    FROM[JOB_PORTAL_DB].[dbo].[Applicant_Educations]";
+
+                conn.Open();
+
+                int x = 0;
+                SqlDataReader rdr = cmd.ExecuteReader();
+                ApplicantEducationPoco[] appPocos = new ApplicantEducationPoco[1000];
+                while (rdr.Read())
+                {
+                    ApplicantEducationPoco poco = new ApplicantEducationPoco();
+                    poco.Id = rdr.GetGuid(0);
+                    poco.Applicant = rdr.GetGuid(1);
+                    poco.Major = rdr.GetString(2);
+                    poco.CertificateDiploma = rdr.GetString(3);
+                    poco.StartDate = rdr.GetDateTime(4);
+                    poco.CompletionDate = rdr.GetDateTime(5);
+                    poco.CompletionPercent = (byte?)rdr[6];
+                    poco.TimeStamp = (byte[])rdr[7];
+
+                    appPocos[x] = poco;
+                    x++;
+                }
+                return appPocos.Where(a => a != null).ToList();
+            }
+        }
+        //    public IList<ApplicantEducationPoco> GetList(Expression<Func<ApplicantEducationPoco, bool>> where, params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public ApplicantEducationPoco GetSingle(Expression<Func<ApplicantEducationPoco, bool>> where, params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void Remove(params ApplicantEducationPoco[] items)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public void Update(params ApplicantEducationPoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                foreach (ApplicantEducationPoco item in items)
+                {
+                    cmd.CommandText = @"UPDATE [dbo].[Applicant_Educations]
+                       SET [Applicant] = @Applicant
+                          ,[Major] = @Major
+                          ,[Certificate_Diploma] = @Certificate_Diploma
+                          ,[Start_Date] = @Start_Date
+                          ,[Completion_Date] = @Completion_Date
+                          ,[Completion_Percent] = @Completion_Percent
+                       WHERE [Id] = @Id";
+
+                    cmd.Parameters.AddWithValue("@Applicant", item.Applicant);
+                    cmd.Parameters.AddWithValue("@Major", item.Major);
+                    cmd.Parameters.AddWithValue("@Certificate_Diploma", item.CertificateDiploma);
+                    cmd.Parameters.AddWithValue("@Start_Date", item.StartDate);
+                    cmd.Parameters.AddWithValue("@Completion_Date", item.CompletionDate);
+                    cmd.Parameters.AddWithValue("@Completion_Percent", item.CompletionPercent);
+                    cmd.Parameters.AddWithValue("@Id", item.Id);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ CareerCloud.ADODataAccessLayer/BaseADORepository.cs
@@ -0,0 +1,64 @@
+﻿using CareerCloud.Pocos;
+using Microsoft.Extensions.Configuration;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public abstract class BaseADORepository<T> where T : IPoco
+    {
+        protected readonly string _connStr ;
+
+        public abstract IList<T> GetAll(params Expression<Func<T, object>>[] navigationProperties);
+
+        public BaseADORepository()
+        {
+            var config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            config.AddJsonFile(path, false);
+            var root = config.Build();
+            _connStr = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+        }
+
+        public IList<T> GetList(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            IQueryable<T> pocos = GetAll().AsQueryable();
+            return pocos.Where(where).ToList();
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            IQueryable<T> pocos = GetAll().AsQueryable();
+            return pocos.Where(where).FirstOrDefault();
+        }
+
+        public virtual void Remove(params T[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                SqlCommand cmd = new SqlCommand
+                {
+                    Connection = conn
+                };
+                foreach (T item in items)
+                {
+                    TableAttribute attribute =
+                        (TableAttribute)Attribute.GetCustomAttribute(item.GetType(), typeof(TableAttribute));
+                    string tblName = attribute.Name;
+
+                    cmd.CommandText = $"DELETE {tblName} WHERE ID = @ID";
+                    cmd.Parameters.AddWithValue("@ID", item.Id);
+                    conn.Open();
+                    int rowEffected = cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+        public virtual void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
